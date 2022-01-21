8eed1bd1f67ded676697f97a3ba6ece388f4962d
Ted Conbeer <ted@shandy.io>
2022-01-21 08:15:54 -0700

    chore: apply sqlfmt v0.4.1

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -1,25 +1,18 @@
-with source as (
-    
+with
     {#-
     Normally we would select from the table here, but we are using seeds to load
     our data in this project
     #}
-    select * from {{ ref('raw_payments') }}
+    source as (select * from {{ ref('raw_payments') }}),
 
-),
-
-renamed as (
-
-    select
-        id as payment_id,
-        order_id,
-        payment_method,
+    renamed as (
 
         -- `amount` is currently stored in cents, so we convert it to dollars
-        amount / 100 as amount
+        select id as payment_id, order_id, payment_method, amount / 100 as amount
 
-    from source
+        from source
 
-)
+    )
 
-select * from renamed
+select *
+from renamed
